165ced71b4dd34eff4ee19365e85c766855c2693
Yoki Yu <yokiyuqian@gmail.com>
2021-09-06 18:44:09 +0800

    init project

---
+++ packages/app/src/containers/SecondPage/index.jsx
@@ -0,0 +1,15 @@
+/**
+ * SecondPage
+ */
+
+import Center from 'components/Center'
+
+function SecondPage() {
+  return (
+    <Center className='SecondPage'>
+      <h1>SecondPage</h1>
+    </Center>
+  )
+}
+
+export default SecondPage
